81e3a91e515d35a0c18d9fd9791a75382897d5dc
Mark Chavez <mjfchavez@gmail.com>
2020-08-17 18:24:10 +0800

    feat: working stats builder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const totalStatCount = 1250 * 3
+const defaultPoint   = 1
+const defaultStat    = 1
 
 function App() {
+
+  const [total, setTotal]     = useState(totalStatCount);
+  const [point, setPoint]     = useState(defaultPoint);
+  const [melee, setMelee]     = useState(defaultStat);
+  const [defense, setDefense] = useState(defaultStat);
+  const [sword, setSword]     = useState(defaultStat);
+  const [gun, setGun]         = useState(defaultStat);
+  const [blox, setBlox]       = useState(defaultStat);
+
+  const increaseStat = (currentValue, setFunc) => {
+    let newValue = parseInt(currentValue) + parseInt(point)
+
+    if (point > total) {
+      return
+    }
+
+    setFunc(newValue)
+    setTotal(total - point)
+  }
+
+  const refundStat = () => {
+    setTotal(totalStatCount)
+    setPoint(defaultPoint)
+    setMelee(defaultStat)
+    setDefense(defaultStat)
+    setSword(defaultStat)
+    setGun(defaultStat)
+    setBlox(defaultStat)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center">
+      <div>
+        <div>
+          <header className="py-4">
+            <span className="suez text-6xl block leading-none"> Blox Piece </span>
+            <span className="suez uppercase block"> 
+              by <a href="https://www.roblox.com/home" target="_blank" className="underline">roblox</a> 
+            </span>
+          </header>
+
+          <div className="border-2">
+            <div className="px-4 py-1 flex items-center bg-black">
+              <span className="flex-1 block uppercase text-white text-4xl font-bold"> Stats </span>
+              <div className="w-1/6">
+                <input 
+                  type="text" 
+                  className="w-full h-12 text-4xl text-center"
+                  value={point} onChange={(e) => setPoint(e.target.value)} />
+              </div>
+            </div>
+
+            <div className="text-4xl">
+              <div className="px-4 py-2 flex items-center"> 
+                <div className="flex-1 flex justify-between">
+                  <span className="text-red-700 font-bold"> Melee </span>
+                  <span className="pr-3"> Lv. {melee} </span>
+                </div>
+            
+                <div className="w-1/6">
+                  <button 
+                    className="w-full bg-yellow-400 hover:bg-yellow-500" 
+                    onClick={() => increaseStat(melee, setMelee)}> 
+                    <span className="font-bold"> + </span> 
+                  </button>
+                </div>
+              </div>
+
+              <div className="px-4 py-2 flex items-center"> 
+                <div className="flex-1 flex justify-between">
+                  <span className="text-blue-700 font-bold"> Defense </span>
+                  <span className="pr-3"> Lv. {defense} </span>
+                </div>
+            
+                <div className="w-1/6">
+                  <button 
+                    className="w-full bg-yellow-400 hover:bg-yellow-500" 
+                    onClick={() => increaseStat(defense, setDefense)}> 
+                    <span className="font-bold"> + </span> 
+                  </button>
+                </div>
+              </div>
+
+              <div className="px-4 py-2 flex items-center"> 
+                <div className="flex-1 flex justify-between">
+                  <span className="text-green-700 font-bold"> Sword </span>
+                  <span className="pr-3"> Lv. {sword} </span>
+                </div>
+            
+                <div className="w-1/6">
+                  <button 
+                    className="w-full bg-yellow-400 hover:bg-yellow-500" 
+                    onClick={() => increaseStat(sword, setSword)}> 
+                    <span className="font-bold"> + </span> 
+                  </button>
+                </div>
+              </div>
+
+              <div className="px-4 py-2 flex items-center"> 
+                <div className="flex-1 flex justify-between">
+                  <span className="text-yellow-700 font-bold"> Gun </span>
+                  <span className="pr-3"> Lv. {gun} </span>
+                </div>
+            
+                <div className="w-1/6">
+                  <button 
+                    className="w-full bg-yellow-400 hover:bg-yellow-500" 
+                    onClick={() => increaseStat(gun, setGun)}> 
+                    <span className="font-bold"> + </span> 
+                  </button>
+                </div>
+              </div>
+
+              <div className="px-4 py-2 flex items-center"> 
+                <div className="flex-1 flex justify-between">
+                  <span className="text-purple-700 font-bold"> Blox Fruit </span>
+                  <span className="pr-3"> Lv. {blox} </span>
+                </div>
+            
+                <div className="w-1/6">
+                  <button 
+                    className="w-full bg-yellow-400 hover:bg-yellow-500" 
+                    onClick={() => increaseStat(blox, setBlox)}> 
+                    <span className="font-bold"> + </span> 
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="px-4 py-1 flex items-center justify-between bg-black text-2xl">
+              <span className="text-white tracking-wide font-bold"> Available Points: {total} </span>
+
+              <div>
+                <button 
+                  className="w-ful bg-yellow-400 hover:bg-yellow-500"
+                  onClick={() => refundStat()}>
+                  <span className="p-2 font-bold"> Refund </span> 
+                </button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
